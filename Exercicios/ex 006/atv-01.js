6e38918ebdec93da514f10aff2b2add1601d93ba
Gabriel Alves <156039989+Alvesgb@users.noreply.github.com>
2024-02-15 16:36:54 -0300

    ex 007

---
+++ Exercicios/ex 006/atv-01.js
@@ -0,0 +1,7 @@
+var vel = 
+console.log(`Velocidade do veiculo: ${vel}km/h`)
+if (vel > 80) {
+    console.log(`Você ultrapassou a velocidade permitida e foi multado!`)
+} else {
+    console.log(`Você esta na velocidade permitida da via, tenha uma boa viajem!`)
+}
