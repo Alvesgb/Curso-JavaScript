537f24c0ba16da5adfcc0d65a6aced9a722730cf
Gabriel Alves <156039989+Alvesgb@users.noreply.github.com>
2024-02-27 16:46:57 -0300

    horario

--- Desafios/Horario/script.js
+++ Desafios/Horario/script.js
@@ -9,15 +9,15 @@ function carregar(){
     msg1.innerHTML = `Agora são ${hr}:${mn}`
 
     if (hr >= 0 && hr < 12) {
-        img.src = '../img/manha.png'
+        img.src = 'img/manha.png'
         msg2.innerHTML = `Bom dia!`
         document.body.style.backgroundColor = '#cfb2a0'
     } else if(hr >=12 && hr <18){
-        img.src = '../img/tarde.png'
+        img.src = 'img/tarde.png'
         msg2.innerHTML = `Boa tarde!`
         document.body.style.backgroundColor = '#845032'
     } else{
-        img.src =  '../img/noite.png'
+        img.src =  'img/noite.png'
         msg2.innerHTML = `Boa noite!`
         document.body.style.backgroundColor = '#5d6c97'
     }
